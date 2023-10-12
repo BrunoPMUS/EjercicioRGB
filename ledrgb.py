48551a1523fda536acdc2b321208af3439aa2f4a
Bruno <bruno2000perez@yahoo.com>
2023-10-11 19:06:50 -0600

    Tercera versión

--- ledrgb.py
+++ ledrgb.py
@@ -3,9 +3,14 @@ import time
 puertosalida = 17
 lr = LED(puertosalida)
 
+def prender(color)
+
+
 parpadeando = True
 while parpadeando:
-    input("Ingrese el led que se quiere prender 'R','G','B' ")
+    if input("Ingrese el led que se quiere prender 'R','G','B' ").upper == "r":
+        lr.on()
+
     lr.on()
     time.sleep(1)
     lr.off()
