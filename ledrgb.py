e5e136cb126a280de699aca606d4e60502d4e4de
Bruno <bruno2000perez@yahoo.com>
2023-10-11 19:05:35 -0600

    Codigo 2

--- ledrgb.py
+++ ledrgb.py
@@ -1,9 +1,11 @@
 from gpiozero import LED
 import time
+puertosalida = 17
+lr = LED(puertosalida)
 
-lr=LED(puertosalida)
 parpadeando = True
 while parpadeando:
+    input("Ingrese el led que se quiere prender 'R','G','B' ")
     lr.on()
     time.sleep(1)
     lr.off()
